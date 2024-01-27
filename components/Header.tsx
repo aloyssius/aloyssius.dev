f07b1b0105edfc76a7e12b1feef8b8256ad76f3e
codingaslife <hatran.devs@gmail.com>
2024-01-27 15:21:12 +0700

    add group hover header icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
     <header className="flex items-center justify-between py-7">
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between group">
             <HeaderMainIcon />
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
